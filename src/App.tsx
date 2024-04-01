157fce7a029aaacb3d010a4e5fc901d2ffec2e52
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-04-02 01:15:21 +0800

    Change module to Vite

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import OutlinedCard from './components/Card'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function App() {
+  return (
+    <Box sx={{ 
+      backgroundColor: 'black',
+      padding: '10px 100px'
+    }}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Item>BACKLOG</Item>
+
+          <OutlinedCard />
+        </Grid>
+        <Grid item xs={3}>
+          <Item>TODO</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>INP ROGRESS</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>DONE</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="body1">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+}
